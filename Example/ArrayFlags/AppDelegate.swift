b669a5a7171a0159cc860d8bc9329e77e3ac5227
tkgka <ksh17171@gmail.com>
2022-03-18 18:38:21 +0900

    Initial commit

---
+++ Example/ArrayFlags/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ArrayFlags
+//
+//  Created by tkgka on 03/18/2022.
+//  Copyright (c) 2022 tkgka. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Example/ArrayFlags/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ArrayFlags
+//
+//  Created by tkgka on 03/18/2022.
+//  Copyright (c) 2022 tkgka. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
